954749dfa9f41eecc03882fdf240cf13843cc987
Christian2650 <128227321+Christian2650@users.noreply.github.com>
2023-06-03 16:09:20 +0200

    Add files via upload

---
+++ 3_BMI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double w, h;
+
+        Console.WriteLine("Prosze podac wage w kg oraz wzrost w m");
+        w = inputValue();
+        h = inputValue();
+        double bMI;
+
+        bMI = w / Math.Pow(h, 2);
+        Console.WriteLine("Body Mass Index =");
+        Console.WriteLine(bMI);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
+
