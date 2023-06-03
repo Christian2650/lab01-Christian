b7bc42bb8897555da53063636b16d46633783f38
Christian2650 <128227321+Christian2650@users.noreply.github.com>
2023-06-03 15:53:43 +0200

    Add files via upload

---
+++ 2_obwod_kola.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double p, o, r;
+
+        p = inputValue();
+        r = Math.Sqrt(p * 3.14);
+        o = 2 * 3.14 * r;
+        Console.WriteLine(o);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
