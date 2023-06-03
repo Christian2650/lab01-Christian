6268355d147e46578b29829449e427fe6a54299f
Christian2650 <128227321+Christian2650@users.noreply.github.com>
2023-06-03 18:33:14 +0200

    Add files via upload

---
+++ 6_trojkat_rownoboczny.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+
+        Console.WriteLine("Proszę podać długość boków trójkąta");
+        Console.WriteLine("Bok 1");
+        a = inputValue();
+        Console.WriteLine("Bok 2");
+        b = inputValue();
+        Console.WriteLine("Bok 3");
+        c = inputValue();
+        if (a == b)
+        {
+            if (a == c)
+            {
+                Console.WriteLine("Podane boki są równie, przez co utworzenie trójkąta równobocznego jest możliwe");
+            }
+            else
+            {
+                Console.WriteLine("Podane boki nie są równe, przez co utworzenie trójkąta równobocznego nie będzie możliwe");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Podane boki nie są równe, przez co utworzenie trójkąta równobocznego nie będzie możliwe");
+        }
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
+
